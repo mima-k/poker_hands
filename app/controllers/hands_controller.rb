3633a93bd856302c34c9faa82a4809796f5936a2
mima-k <mimakouhei@gmail.com>
2021-03-30 19:01:30 +0900

    エラーメッセージの表示

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -1,10 +1,13 @@
 class HandsController < ApplicationController
   protect_from_forgery except: :index
+
   def index
     @hand = Hand.new
     if request.post?
       @hand = Hand.new(cards: params[:cards])
-      @hand.judge_hands
+      if @hand.valid?
+        @hand.judge_hands
+      end
     end
   end
 end
