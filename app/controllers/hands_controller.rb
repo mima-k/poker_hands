bee11505499d20b0c8bd1548ed9d3a799fc33952
mima-k <mimakouhei@gmail.com>
2021-04-04 00:39:44 +0900

    初期化上書きの修正

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -2,12 +2,13 @@ class HandsController < ApplicationController
   protect_from_forgery except: :index
 
   def index
-    @hand = Hand.new
     if request.post?
       @hand = Hand.new(cards: params[:cards])
       if @hand.valid?
         @hand.judge_hands
       end
+    else
+      @hand = Hand.new
     end
   end
 end
